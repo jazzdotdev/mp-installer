be7e338f3b2bbaebb44091a69142b9aa848e5af2
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-02 06:06:50 +0530

    updated to use symlinks

--- init.lua
+++ init.lua
@@ -8,15 +8,8 @@ if fs.is_dir(mp_dir) then
 end
 
 git.clone( torchbear.settings.git_url, mp_dir )
-
-local cmd = "#!/bin/env bash\ncd " .. mp_dir ..  "; torchbear"
-
+fs.symlink(mp_dir .. "/init.lua", dir .. "mp")
 local cmd_path = dir .. "mp"
-
-local f = io.open(cmd_path, "w")
-f:write(cmd)
-f:close()
-
 os.execute("chmod +x " .. cmd_path)
 
 
