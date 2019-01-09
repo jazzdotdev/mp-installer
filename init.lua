02f0663785afb1772cfea4de0a11abd398c98677
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-09 22:23:11 +0530

    Added support for android

--- init.lua
+++ init.lua
@@ -7,17 +7,20 @@ if torchbear.os == "windows" then
 	-- TODO handle in torchbear
 	mp_dir = os.getenv("CMDER_ROOT") .. mp_dir
 	bin_dir = os.getenv("CMDER_ROOT") .. "/bin/"
+elseif torchbear.os == "android" then
+	bin_dir = "/data/data/com.termux/files/usr/bin/"
+	mp_dir = bin_dir .. "machu-picchu/"
 end
 
 if fs.is_dir(mp_dir) then
-  fs.remove_dir(mp_dir, true)
+	fs.remove_dir(mp_dir, true)
 end
 
 git.clone( torchbear.settings.git_url, mp_dir )
 if torchbear.os == "windows" then
 	fs.copy_file("mp.bat", bin_dir)
 else
-    local cmd_path = bin_dir .. "mp"
+	local cmd_path = bin_dir .. "mp"
 	fs.symlink(mp_dir .. "/init.lua", bin_dir .. "mp")
 	os.execute("chmod +x " .. cmd_path)
 end
